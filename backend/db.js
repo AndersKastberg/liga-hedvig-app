1f221b521963a69dd9ad1a91135e793c29e5302f
Anders Kastberg <anders.kastberg@gmail.com>
2025-02-18 16:48:06 +0100

    More Azure SQL Connect

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql');
-
+const fs = require('fs');
+const ca = fs.readFileSync('cert/DigiCertGlobalRootCA.crt.pem');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'sysadmin',
-    password: 'c@n0n94554248',
-    database: 'ligahedvig'
-});
+  host: 'ligahedvig.mysql.database.azure.com',
+  user: 'sysadmin',
+  password: '8tdB5.XJd!!geJs',
+  database: 'ligahedvig',
+  ssl: {
+      ca: ca
+  } });
 
 connection.connect(err => {
   if (err) throw err;
