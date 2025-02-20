e61c6176ebee4bbeaa4ae2675aa254a772bf6456
Anders Kastberg <anders.kastberg@gmail.com>
2025-02-20 15:25:16 +0100

    Generic passwords

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,5 @@ export const environment = {
     apiUrl: 'https://liga-hedvig-affqgsefdcbqarcy.northeurope-01.azurewebsites.net/',
     allowedDomains: ['liga-hedvig-affqgsefdcbqarcy.northeurope-01.azurewebsites.net'],
     disallowedRoutes: ['https://liga-hedvig-affqgsefdcbqarcy.northeurope-01.azurewebsites.net/login', 'https://liga-hedvig-affqgsefdcbqarcy.northeurope-01.azurewebsites.net/register'],
-    dbHost: 'ligahedvig.mysql.database.azure.com',
-    dbUser: 'sysadmin',
-    dbPassword: '8tdB5.XJd!!geJs',
-    dbDatabase: 'ligahedvig',
+    
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,6 @@ export const environment = {
     apiUrl: 'http://localhost:3000/',
     allowedDomains: ['localhost:3000'],
     disallowedRoutes: ['http://localhost:3000/login', 'http://localhost:3000/register'],
-    dbHost:  'localhost',
-    dbUser: 'sysadmin',
-    dbPassword: 'c@n0n94554248',
-    dbDatabase: 'ligahedvig'
     }
   ;
   
